38ad85ec2ee942ff23929be218651979a76c7f07
Artem Golovin <artem.golovin@justeattakeaway.com>
2021-01-13 13:16:39 -0700

    Initial Commit

---
+++ slowed/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  slowed
+//
+//  Created by Artem Golovin on 2021-01-13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ slowed/slowedApp.swift
@@ -0,0 +1,17 @@
+//
+//  slowedApp.swift
+//  slowed
+//
+//  Created by Artem Golovin on 2021-01-13.
+//
+
+import SwiftUI
+
+@main
+struct slowedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
